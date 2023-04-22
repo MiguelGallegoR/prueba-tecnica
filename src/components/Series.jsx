3440535e78c07b4a0c08ee8071a885d01c659ca8
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-22 16:32:55 +0200

    add Serie, SerieInfo,handleSelectSerie,handleClose

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react'
 
 function ListOfSeries ({ series }) {
+  const [selectedSerie, setSelectedSerie] = useState(null)
+
+  const handleSelectSerie = (serie) => {
+    setSelectedSerie(serie)
+  }
+
+  const handlePopupClose = () => {
+    setSelectedSerie(null)
+  }
+
   return (
     <ul className='series'>
       {
-                series.map(serie => (
-                  <li className='serie' key={serie.title}>
-                    <h3>{serie.title}</h3>
-                    <p>{serie.releaseYear}</p>
-                    <img src={serie.images['Poster Art'].url} alt={serie.title} width='300px' />
-                  </li>
-                ))
-            }
+        series.map(serie =>
+          <Serie serie={serie} key={serie.title} onSelectedSerie={() => handleSelectSerie(serie)} />
+        )
+      }
+      {selectedSerie && <SerieInfo serieInfo={selectedSerie} handlePopupClose={handlePopupClose} />}
+
     </ul>
   )
 }
@@ -27,6 +36,32 @@ export function Series ({ series }) {
     hasSeries
       ? <ListOfSeries series={series} />
       : <NoSeriesResult />
+
+  )
+}
+
+function Serie ({ serie, onSelectedSerie }) {
+  return (
+    <li className='serie' key={serie.title} onClick={onSelectedSerie}>
+      <h3>{serie.title}</h3>
+      <p>{serie.releaseYear}</p>
+      <img src={serie.images['Poster Art'].url} alt={serie.title} width='300px' />
+    </li>
+  )
+}
+
+function SerieInfo ({ serieInfo, handlePopupClose }) {
+  return (
+    <div className='popup'>
+      <div className='popup-content'>
+        <button onClick={handlePopupClose}>Cerrar</button>
+        <h2>{serieInfo.title}</h2>
+        <p>{serieInfo.description}</p>
+        <p>{serieInfo.releaseYear}</p>
+        <img src={serieInfo.images['Poster Art'].url} alt={serieInfo.title} width='300px' />
+      </div>
+
+    </div>
 
   )
 }
