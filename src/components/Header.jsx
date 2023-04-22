bd26cf8bb0ddde6216f69390152d82fb9dd953ed
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-22 16:14:42 +0200

    added Home component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
-export default function Header () {
+import { Home } from './Home'
+
+export default function Header ({ handlePageChange }) {
   return (
-    <header>
-      <div className='header-menu'>
-        <h1>DEMO Streaming</h1>
-        <div className='header-buttons'>
-          <a href=''>Log in</a>
-          <button>Start your free trial</button>
+    <>
+      <header>
+        <div className='header-menu'>
+          <a href='/index'>
+            <h1>DEMO Streaming</h1>
+          </a>
+          <div className='header-buttons'>
+            <a href=''>Log in</a>
+            <button>Start your free trial</button>
+          </div>
         </div>
-      </div>
-      <div id='popular-titles'>
-        <h2 style={{ textAlign: 'left', fontSize: '2rem' }}>Popular titles</h2>
-      </div>
-    </header>
+        <div id='popular-titles'>
+          <h2 style={{ textAlign: 'left', fontSize: '2rem' }}>Popular titles</h2>
+        </div>
+      </header>
+      <Home handlePageChange={handlePageChange} />
+    </>
   )
 }
