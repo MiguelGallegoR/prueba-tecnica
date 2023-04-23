58b6c53e6f88fd39560968d68bab4965e46ac4ce
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-23 13:29:07 +0200

    added imageCheck

--- src/services/getSeries.js
+++ src/services/getSeries.js
@@ -1,9 +1,24 @@
 export const getSeries = async () => {
+  const defaultImageUrl = '../src/assets/clapperboard.png'
   return fetch('http://localhost:3000/entries')
     .then(res => res.json())
-    .then(data => {
+    .then(async data => {
       const allSeries = data.filter(entrie => entrie.programType === 'series' && entrie.releaseYear >= 2010)
       const series = allSeries.slice(0, 20)
-      return series
+      const checkImagePromises = series.map(async serie => {
+        const imageResponse = await fetch(serie.images['Poster Art'].url)
+        return {
+          serie,
+          isImageOk: imageResponse.status !== 404
+        }
+      })
+
+      const imageChecks = await Promise.all(checkImagePromises)
+
+      // Agregar la URL de la imagen o la imagen por defecto a cada película
+      return imageChecks.map(imageCheck => ({
+        ...imageCheck.serie,
+        imageUrl: imageCheck.isImageOk ? imageCheck.serie.images['Poster Art'].url : defaultImageUrl
+      }))
     })
 }
