29a1aec5e932cab867c97e5673eea084e2067820
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-23 17:08:07 +0200

    added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
 import Header from './components/Header'
+import { Home } from './components/Home'
 import { Movies } from './components/Movies'
 import { Series } from './components/Series'
 import { useState } from 'react'
-
+import { Footer } from './components/Footer'
 function App () {
   const [pageLoaded, setPageLoaded] = useState('Home')
 
@@ -13,11 +14,15 @@ function App () {
 
   return (
     <>
-      <Header handlePageChange={handlePageChange} />
-      <main>
+      <Header />
+      <Home handlePageChange={handlePageChange} />
+
+      <main style={{ width: '80%', margin: '0 auto' }}>
         {pageLoaded === 'Movies' && <Movies />}
         {pageLoaded === 'Series' && <Series />}
       </main>
+      {pageLoaded === 'Home' && <Footer />}
+
     </>
   )
 }
