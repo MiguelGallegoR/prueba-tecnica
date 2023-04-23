5eb3c25108796a75a255605f3a5d3e09a09dbe8f
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-23 12:22:02 +0200

    created Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export function Footer () {
+  return (
+    <div>
+        <p>Home |</p>
+        <p>Terms and Conditions |</p>
+        <p>Privacy Policy |</p>
+        <p>Collection Statement |</p>
+        <p>Help |</p>
+        <p>Manage Account |</p>
+    </div>
+    
+  )
+}
